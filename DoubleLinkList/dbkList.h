995eb878a87c7ceccdbcf8b302b9cf62940a5ae9
JackieCooo <43923918+JackieCooo@users.noreply.github.com>
2021-05-30 16:23:47 +0800

    Add files via upload

---
+++ DoubleLinkList/dbkList.h
@@ -0,0 +1,444 @@
+/**
+  ******************************************************************************
+  * @file    lnkList.h
+  * @author  Jackie
+  * @version V1.0
+  * @date    2021-04-07
+  * @brief   双链表模板
+  ******************************************************************************
+  */
+
+#pragma once
+
+#include <iostream>
+#include "Node.h"
+
+using namespace std;
+
+template<class T>
+class lnkList {
+private:
+    Node<T> *head, *tail;  // 头节点，尾节点
+    int len = 0;  // 链表长度
+
+public:
+    lnkList();
+    explicit lnkList(int n);
+    lnkList(int n, int val);
+    ~lnkList();
+    void clear();
+    int size();
+    void push_back(T val);
+    void insert(int p, T val);
+    void erase(int p);
+    void pop_back();
+    bool empty();
+    void pop_front();
+    void push_front(T val);
+    int getPos(T val);
+    T& front();
+    T& back();
+    T* begin();
+    T* end();
+    lnkList<T>& operator=(lnkList<T> a);
+    T& operator[](int p);
+
+private:
+    Node<T>* getNode(int p);
+    void init();
+
+    // 下面定义一些不能在类外定义的函数
+public:
+    /**
+      * @brief  此函数重载<<运算符
+      * @param[in] out ostream的引用
+      * @param[in] a 要操作的类对象
+      * @retval 返回ostream的引用
+      */
+    friend ostream& operator<< (ostream& out, lnkList<T>& a){
+        if(!a.size()){
+            out << "空表" << endl;
+            return out;
+        }
+        Node<T> *pos;
+        pos = a.head->next;
+        for (int i = 0; i < a.size(); ++i) {
+            out << pos->data << " ";
+            pos = pos->next;
+        }
+        out << endl;
+        return out;
+    }
+    /**
+      * @brief  此函数重载>>运算符
+      * @param[in] in istream的引用
+      * @param[in] a 要操作的类对象
+      * @retval 返回istream的引用
+      */
+    friend istream& operator>> (istream& in, T& a){
+        in >> a;
+        return in;
+    }
+
+};
+
+/**
+  * @brief  此函数为构造函数
+  * @param None
+  * @retval None
+  */
+template<class T>
+lnkList<T>::lnkList() {
+    init();
+}
+
+/**
+  * @brief  此构造函数创建一定长度的链表
+  * @param[in] n 创建的链表长度
+  * @retval None
+  */
+template<class T>
+lnkList<T>::lnkList(int n) {
+    init();
+    for (int i = 0; i < n; ++i) {
+        push_back(0);
+    }
+}
+
+/**
+  * @brief  此构造函数创建一定长度的链表并赋相同的值
+  * @param[in] n 要创建链表的长度
+  * @param[in] val 要赋的值
+  * @retval None
+  */
+template<class T>
+lnkList<T>::lnkList(int n, int val) {
+    init();
+    for (int i = 0; i < n; ++i) {
+        push_back(val);
+    }
+}
+
+/**
+  * @brief  此函数为析构函数，清除所有的节点
+  * @param None
+  * @retval None
+  */
+template<class T>
+lnkList<T>::~lnkList() {
+    Node<T> *tmp;  // 定义指向结点的指针
+    while(head != NULL){
+        tmp = head;  // tmp指向当前结点
+        head = head->next;  // head指向下一结点
+        delete tmp;  // 删除当前结点
+    }
+}
+
+/**
+  * @brief  此函数清除链表所有元素
+  * @param None
+  * @retval None
+  */
+template<class T>
+void lnkList<T>::clear(){
+    Node<T> *tmp, *nex;  // tmp为待删除节点，nex为tmp的后驱，防止删除tmp后无法找到后驱
+    tmp = head->next;
+    for (int i = 0; i < size(); ++i) {
+        nex = tmp->next;  // 获取tmp后驱
+        delete tmp;  // 删除tmp
+        tmp = nex;  // 将前后驱赋给tmp
+    }
+    len = 0;
+    head->next = tail;
+    tail->next = head;
+}
+
+/**
+  * @brief  此函数获取链表的长度
+  * @param None
+  * @retval 返回链表长度
+  */
+template<class T>
+int lnkList<T>::size() {
+    return len;
+}
+
+/**
+  * @brief  此函数向链表追加一个元素
+  * @param[in] val 追加元素的值
+  * @retval None
+  */
+template<class T>
+void lnkList<T>::push_back(T val) {
+    Node<T> *temp, *pre_pos;  // temp为插入节点，pre_pos为插入位置前驱节点
+    pre_pos = tail->pre;  // 获取末节点
+    temp = new Node<T>(val, tail, pre_pos);  // temp后驱更新为tail
+    tail->pre = temp;  // 更新tail前驱
+    pre_pos->next = temp;  // 前末节点的后驱更新为新插入节点
+    ++len;  // 长度+1
+    cout << "追加成功" << endl;
+}
+
+/**
+  * @brief  此函数往链表指定位置插入一个元素
+  * @param[in] p 插入的位置（0 <= p <= len）
+  * @param[in] val 插入元素的值
+  * @retval None
+  */
+template<class T>
+void lnkList<T>::insert(int p, T val) {
+    if (p < 0 || p > size()) {  // 不能往头结点前或尾节点后插值
+        cout << "位置错误" << endl;
+        return;
+    }
+    else if(p == size()){  // 在最后面插入
+        push_back(val);
+        return;
+    }
+    else if(p == 0){  // 在最前面插入
+        push_front(val);
+        return;
+    }
+    else{
+        Node<T> *pre_pos, *temp;  // temp为待插入节点，pre_pos为插入位置的前一个节点
+
+        pre_pos = getNode(p-1);  // 获取插入位置的前一个节点
+        temp = new Node<T>(val, pre_pos->next, pre_pos);  // temp的下一个节点设置为pre_pos的下一个节点，上一个节点为pre_pos
+        pre_pos->next = temp;  // pre_pos的后驱指针更新为temp
+        (temp->next)->pre = temp;  // 原pre_pos的后驱节点的前驱指针更新为temp
+        ++len;  // 长度+1
+        cout << "追加成功" << endl;
+    }
+}
+
+/**
+  * @brief  此函数删除链表指定位置的一个元素
+  * @param[in] p 删除元素的位置（0 <= p < len）
+  * @retval None
+  */
+template<class T>
+void lnkList<T>::erase(int p) {
+    if(empty()){
+        cout << "表空！" << endl;
+        return;
+    }
+
+    if(p < 0 || p >= size()){
+        cout << "删除位置错误" << endl;
+        return;
+    }
+    else if(p == 0){  // 删除第一个节点
+        pop_front();
+        return;
+    }
+    else if(p == size()-1){  // 删除尾节点
+        pop_back();
+        return;
+    }
+    else{
+        Node<T> *temp, *pre_pos;  // temp为要删除的节点，pre_pos为要删除节点的前一个节点
+
+        temp = getNode(p);  // 获取删除节点
+        pre_pos = temp->pre;
+        pre_pos->next = temp->next;  // pre_pos的后驱更新为temp的后驱
+        (temp->next)->pre = pre_pos;  // temp的后驱的前驱指针更新为pre_pos
+        delete temp;
+        --len;  // 长度-1
+        cout << "删除成功" << endl;
+    }
+}
+
+/**
+  * @brief  此函数删除链表最后一个元素
+  * @param None
+  * @retval None
+  */
+template<class T>
+void lnkList<T>::pop_back() {
+    if(empty()){
+        cout << "表空！" << endl;
+        return;
+    }
+
+    Node<T> *pre_pos, *temp;  // pre_pos为末节点前驱节点，temp为末节点
+    temp = tail->next;  // 获取末节点
+    pre_pos = temp->pre;  // 获取末节点前驱节点
+    pre_pos->next = tail;  // 前末节点前驱节点更新为末节点
+    tail->pre = pre_pos;  // 末节点更新为pre_pos
+    delete temp;
+    --len;
+    cout << "删除成功" << endl;
+}
+
+/**
+  * @brief  此函数获取链表是否为空的判断
+  * @param None
+  * @retval true 链表为空
+  * @retval false 链表非空
+  */
+template<class T>
+bool lnkList<T>::empty() {
+    return !size();
+}
+
+/**
+  * @brief  此函数删除链表第一个元素
+  * @param None
+  * @retval None
+  */
+template<class T>
+void lnkList<T>::pop_front() {
+    Node<T> *temp;  // temp为删除节点
+    temp = head->next;
+    head->next = temp->next;  // 更新头节点为temp的前驱
+    (temp->next)->pre = head->next;  // 更新首节点后驱的前驱指针
+    delete temp;
+    --len;
+    cout << "删除成功" << endl;
+}
+
+/**
+  * @brief  此函数在链表最前面追加一个元素
+  * @param[in] val 追加元素的值
+  * @retval None
+  */
+template<class T>
+void lnkList<T>::push_front(T val) {
+    Node<T> *temp;  // temp为插入节点
+    temp = new Node<T>(val, head->next, head);
+    (head->next)->pre = temp;  // 更新首节点后驱节点的前驱指针
+    head->next = temp;  // 更新头节点后驱指针
+    ++len;
+    cout << "插入成功" << endl;
+}
+
+/**
+  * @brief  此函数获取链表元素的位置（返回找到的第一个元素的位置）
+  * @param[in] val 要寻找的元素
+  * @retval 返回对应元素的位置，找不到返回-1
+  */
+template<class T>
+int lnkList<T>::getPos(T val) {
+    Node<T> *temp;
+    temp = head;
+    for (int i = 0; i < size(); ++i) {
+        temp = temp->next;
+        if (temp->data == val){
+            return i;
+        }
+    }
+    return -1;
+}
+
+/**
+  * @brief  此函数返回链表第一个元素的引用
+  * @param None
+  * @retval 返回链表第一个元素的引用
+  */
+template<class T>
+T& lnkList<T>::front() {
+    return (head->next)->data;
+}
+
+/**
+  * @brief  此函数返回链表最后一个元素的引用
+  * @param None
+  * @retval 返回链表最后一个元素的引用
+  */
+template<class T>
+T& lnkList<T>::back() {
+    return (tail->pre)->data;
+}
+
+/**
+  * @brief  此函数返回链表第一个元素的指针
+  * @param None
+  * @retval 返回链表第一个元素的指针
+  */
+template<class T>
+T* lnkList<T>::begin() {
+    return head->next;
+}
+
+/**
+  * @brief  此函数返回链表最后一个元素的指针
+  * @param None
+  * @retval 返回链表最后一个元素的指针
+  */
+template<class T>
+T* lnkList<T>::end() {
+    return tail->pre;
+}
+
+/**
+  * @brief  此函数重载=运算符，完成两个链表间的复制
+  * @param[in] a 要复制的链表
+  * @retval 返回当前链表的引用
+  */
+template<class T>
+lnkList<T>& lnkList<T>::operator=(lnkList<T> a) {
+    clear();  // 清除原来的链表
+    Node<T> *temp;
+    temp = a.head->next;
+    for (int i = 0; i < a.size(); ++i) {
+        push_back(temp->data);
+        temp = temp->next;
+    }
+}
+
+/**
+  * @brief  此函数重载[]运算符，获取链表指定位置元素的引用
+  * @param[in] p 元素的位置
+  * @retval 返回链表指定元素的引用
+  */
+template<class T>
+T& lnkList<T>::operator[](int p){
+    if(p < 0 || p > size()-1){
+        cout << "位置错误" << endl;
+        return head->next->data;
+    }
+    Node<T> *res;
+    res = getNode(p);  // 获取指定节点
+    return res->data;
+}
+
+/**
+  * @brief  此函数获取链表指定位置的节点指针
+  * @param[in] p 元素位置
+  * @retval 返回链表指定位置节点的指针
+  */
+template<class T>
+Node<T>* lnkList<T>::getNode(int p) {
+    if(p < 0 || p > size()) return head;  // 越界返回头节点
+    Node<T> *temp;
+    if(p <= size()/2){  // 由p的位置决定前搜还是后搜
+        temp = head->next;
+        for (int i = 0; i < p; ++i) {
+            temp = temp->next;
+        }
+        return temp;
+    }
+    else{
+        temp = tail->pre;
+        for (int i = size()-1; i >= p; --i) {
+            temp = temp->pre;
+        }
+        return temp;
+    }
+}
+
+/**
+  * @brief  此函数初始化链表的变量
+  * @param None
+  * @retval None
+  */
+template<class T>
+void lnkList<T>::init() {
+    len = 0;  // 初始化长度为0
+    head = new Node<T>;  // 创建头结点
+    tail = new Node<T>;  // 创建尾结点
+    head->next = tail;  // 头结点后驱为尾节点
+    head->pre = nullptr;  // 头节点前驱为空
+    tail->next = nullptr;  // 尾结点后驱为空
+    tail->pre = head;  // 尾节点前驱为头节点
+}
---
+++ DoubleLinkList/main.cpp
@@ -0,0 +1,127 @@
+/**
+  ******************************************************************************
+  * @file    main.cpp
+  * @author  Jackie
+  * @version V1.0
+  * @date    2021-04-07
+  * @brief   主程序
+  ******************************************************************************
+  * @attention
+  * C++标准：C++20
+  * IDE：Clion 2020.2
+  * .exe文件在cmake-build-debug文件内
+  *
+  * 代码已上传至Github和码云，关注日后更新
+  * Github：https://github.com/JackieCooo/dbkList
+  * 码云：https://gitee.com/JackieCoo/dbkList
+  ******************************************************************************
+  */
+
+#include <iostream>
+#include "dbkList.h"
+
+using namespace std;
+
+/**
+  * @brief  主函数
+  * @param None
+  * @retval None
+  */
+int main() {
+    // 自己根据需要选择不同的编译方案
+#if 0  // 这里为用户代码
+    lnkList<int> list;
+    list.push_back(1);
+    list.push_back(2);
+    list.push_back(3);
+    list.insert(1, 4);
+    cout << list;
+#else  // 这里为测试代码
+    lnkList<int> *ptr;
+    ptr = nullptr;
+    int choice;
+    int p;
+    int value;
+    while (true) {
+        cout << "请选择(0退出, 1清除, 2显示, 3表长, 4插入, 5追加, 6删除, 7查找, 8修改, 9定位, 10创建, 11销毁):";
+
+        cin >> choice;
+        switch (choice) {
+            case 0: {
+                system("cls");  // 清屏
+                cout << "再见！" << endl;
+                system("pause");  // 暂停
+                exit(0);  // 退出程序
+            }
+            case 1: {
+                ptr->clear();
+                break;
+            }
+            case 2: {
+                cout << *ptr;
+                break;
+            }
+            case 3: {
+                cout << "表长:" << ptr->size() << endl;
+                break;
+            }
+            case 4: {
+                cout << "位置:";
+                cin >> p;
+                cout << "元素:";
+                cin >> value;
+                ptr->insert(p, value);
+                break;
+            }
+            case 5: {
+                printf("元素:");
+                cin >> value;
+                ptr->push_back(value);
+                break;
+            }
+            case 6: {
+                cout << "位置:";
+                cin >> p;
+                ptr->erase(p);
+                break;
+            }
+            case 7: {
+                cout << "位置:";
+                cin >> p;
+                cout << "元素为：" << (*ptr)[p] << endl;
+                break;
+            }
+            case 8: {
+                cout << "位置:";
+                cin >> p;
+                cout << "元素:";
+                cin >> (*ptr)[p];
+                break;
+            }
+            case 9: {
+                cout << "元素:";
+                cin >> value;
+                cout << "元素位置为:" << ptr->getPos(value) << endl;
+                break;
+            }
+            case 10: {
+                if (ptr != nullptr){
+                    delete ptr;
+                    ptr = nullptr;
+                }
+                ptr = new lnkList<int>;
+                cout << "创建成功" << endl;
+                break;
+            }
+            case 11:{
+                delete ptr;
+                ptr = nullptr;
+                cout << "删除成功" << endl;
+                break;
+            }
+            default: cout << "选择错误！" << endl;
+        }
+    }
+#endif
+    exit(0);
+}
